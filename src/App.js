54e6f5072b87dbb1add63e7aea403bf849ce2ae0
Kiran TS <65617692+tskiran4@users.noreply.github.com>
2024-01-27 15:57:09 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
         <p>
           Kiran T S
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        // <a
+        //   className="App-link"
+        //   href="https://reactjs.org"
+        //   target="_blank"
+        //   rel="noopener noreferrer"
+        // >
+        //   Learn React
+        // </a>
       </header>
     </div>
   );
