0a641419bf169d865f9c0f155e4c615fc0fb1665
tskiran4 <tskiran4@gmail.com>
2024-01-27 16:16:02 +0000

    Push test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kiran T S
+          Push Test
         </p>
       </header>
     </div>
